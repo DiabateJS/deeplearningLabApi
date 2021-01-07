661f62c35bdf8697c965a29635aafd07c0b0c043
DiabateJS <diabatejs@yahoo.fr>
2021-01-07 21:47:08 +0100

    Ajout des paramétres de la prod.

--- models/constants.php
+++ models/constants.php
@@ -2,10 +2,10 @@
 
 class Constants {
     //DATABASE CONSTANTS
-    public static $BD_NAME = "deeplearning";
-    public static $BD_HOST_NAME = "localhost";
-    public static $BD_USER_NAME = "root";
-    public static $BD_USER_PASSWORD = "";
+    public static $BD_NAME = "djste1070339";
+    public static $BD_HOST_NAME = "185.98.131.90";
+    public static $BD_USER_NAME = "djste1070339";
+    public static $BD_USER_PASSWORD = "da6ysjpqpp";
 
 
     public static $EMPTY_STRING = "";
@@ -14,22 +14,22 @@ class Constants {
     public static $ERROR_CODE = "ERROR";
 
     //SQL NETWORK
-    public static $SQL_SELECT_NETWORKS = "select * from network order by id";
-    public static $SQL_SELECT_NETWORK = "select * from network where id = :idNetwork";
-    public static $SQL_UPDATE_NETWORK = "update network set label = :label , neuronsParCouches = :neuronsParCouches , tauxApprentissage = :tauxApprentissage, fonctionTransfert = :fonctionTransfert, typeReseau = :typeReseau where id = :idNetwork";
-    public static $SQL_CREATE_NETWORK = "insert into network (label, neuronsParCouches, tauxApprentissage, fonctionTransfert, typeReseau) values (:label, :neuronsParCouches, :tauxApprentissage, :fonctionTransfert, :typeReseau)";
-    public static $SQL_DELETE_NETWORK = "delete from network where id = :idNetwork";
+    public static $SQL_SELECT_NETWORKS = "select * from dl_network order by id";
+    public static $SQL_SELECT_NETWORK = "select * from dl_network where id = :idNetwork";
+    public static $SQL_UPDATE_NETWORK = "update dl_network set label = :label , neuronsParCouches = :neuronsParCouches , tauxApprentissage = :tauxApprentissage, fonctionTransfert = :fonctionTransfert, typeReseau = :typeReseau where id = :idNetwork";
+    public static $SQL_CREATE_NETWORK = "insert into dl_network (label, neuronsParCouches, tauxApprentissage, fonctionTransfert, typeReseau) values (:label, :neuronsParCouches, :tauxApprentissage, :fonctionTransfert, :typeReseau)";
+    public static $SQL_DELETE_NETWORK = "delete from dl_network where id = :idNetwork";
 
     //SQL LEARNING CONFIG
-    public static $SQL_SELECT_LEARNING_CONFIG = "select *  from learningconfig";
-    public static $SQL_UPDATE_LEARNING_CONFIG = "update from learningconfig set baseItem = :baseItem, learningRate = :learningRate, learningAverage = :learningAverage, ecartMoy = :ecartMoy where id = :id";
+    public static $SQL_SELECT_LEARNING_CONFIG = "select *  from dl_learningconfig";
+    public static $SQL_UPDATE_LEARNING_CONFIG = "update from dl_learningconfig set baseItem = :baseItem, learningRate = :learningRate, learningAverage = :learningAverage, ecartMoy = :ecartMoy where id = :id";
 
     //SQL LEARNING BASE ITEMS
-    public static $SQL_SELECT_NETWORK_LEARNING_BASE_ITEMS = "select * from baseentrainement where idNetwork = :idNetwork";
-    public static $SQL_SELECT_NETWORK_LEARNING_BASE_ITEM = "select * from baseentrainement where idNetwork = :idNetwork and id = :idItem";
-    public static $SQL_UPDATE_NETWORK_LEARNING_BASE_ITEM = "update baseentrainement set input = :input, output = :output where idNetwork = :idNetwork and id = :idItem";
-    public static $SQL_CREATE_NETWORK_LEARNING_BASE_ITEM = "insert into baseentrainement (input, output, idNetwork) values (:input, :output, :idNetwork)";
-    public static $SQL_DELETE_NETWORK_LEARNING_BASE_ITEM = "delete from baseentrainement where id = :idItem";
+    public static $SQL_SELECT_NETWORK_LEARNING_BASE_ITEMS = "select * from dl_baseentrainement where idNetwork = :idNetwork";
+    public static $SQL_SELECT_NETWORK_LEARNING_BASE_ITEM = "select * from dl_baseentrainement where idNetwork = :idNetwork and id = :idItem";
+    public static $SQL_UPDATE_NETWORK_LEARNING_BASE_ITEM = "update dl_baseentrainement set input = :input, output = :output where idNetwork = :idNetwork and id = :idItem";
+    public static $SQL_CREATE_NETWORK_LEARNING_BASE_ITEM = "insert into dl_baseentrainement (input, output, idNetwork) values (:input, :output, :idNetwork)";
+    public static $SQL_DELETE_NETWORK_LEARNING_BASE_ITEM = "delete from dl_baseentrainement where id = :idItem";
 
     //SQL STATISTIQUE
     public static $SQL_STATE_BY_USERS = "select u.fullname, t.etat, count(*) as tache from tache t, users u where t.idUser = u.id group by t.idUser, t.etat order by u.fullname";
